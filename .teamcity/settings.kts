152b778fab1dbccca9875f73ca74bf079828e499
Valravn <Valravn@TeamCity>
2023-06-05 12:43:06 +0000

    TeamCity change in 'Chains Sample (.NET)' project: general settings of 'Build All' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -43,7 +43,6 @@ object Building_1 : BuildType({
     name = "Build All"
 
     artifactRules = "bin/**/*.* => ."
-    type = BuildTypeSettings.Type.COMPOSITE
 
     vcs {
         root(DslContext.settingsRoot)
