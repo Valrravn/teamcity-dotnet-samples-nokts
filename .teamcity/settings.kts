f908af0fd048dc5831fad49d2542e0e336f59f83
Valravn <Valravn@TeamCity>
2023-06-08 14:41:24 +0000

    TeamCity change in 'Chains Sample (.NET) / Deployment Configurations' project: runners of 'Deploy Console (Windows)' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -469,8 +469,8 @@ object DeploymentConfigsProject_DeployConsoleWindows : BuildType({
                 }
                 contextDir = "context"
                 platform = DockerCommandStep.ImagePlatform.Windows
-                namesAndTags = "valrravn/clock-console:nanoserver.ltsc2022"
-                commandArgs = "--build-arg baseImage=mcr.microsoft.com/windows/nanoserver:ltsc2022"
+                namesAndTags = "valrravn/clock-console:windows"
+                commandArgs = "--build-arg baseImage=mcr.microsoft.com/dotnet/runtime-deps:7.0"
             }
         }
         dockerCommand {
