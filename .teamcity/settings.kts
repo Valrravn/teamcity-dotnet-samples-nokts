83b63a249d688ae1a7d6201ff2878e6bfd38d9a4
Valravn <Valravn@TeamCity>
2023-06-13 09:55:14 +0000

    TeamCity change in 'Chains Sample (.NET) / Building Configurations' project: 'WinConsole + Parallel Tests' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -348,7 +348,6 @@ object Building_BuildingWinDesktopWithParallelTesting : BuildType({
     steps {
         dotnetTest {
             name = "Test Win"
-            enabled = false
             projects = "Clock.Tests/Clock.Tests.csproj"
             sdk = "7"
             dockerImage = "mcr.microsoft.com/dotnet/sdk:7.0"
